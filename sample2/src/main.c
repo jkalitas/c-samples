2b7d06aaa93ef30d357a19fe031d91314b551924
Jose Silva <jsilva@codavel.com>
2018-12-06 21:30:43 +0000

    Sample 2

--- sample2/src/main.c
+++ sample2/src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#define VALUE 5
 
+int globalnum; //declaration of a global value
+
+/**
+ * This is a basic comment block.
+ * @return the result of the program
+ */
 int main() {
-    printf("Hello, World!\n");
+    int some_int,other_int;
+    long long_value;
+
+    globalnum = VALUE;
+    some_int = VALUE;
+    other_int = VALUE;
+    long_value = VALUE;
+
+    long result = globalnum * some_int * other_int * long_value;
+    printf("Product of all variables is = %ld\n",result);
     return 0;
 }
